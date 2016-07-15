30e8209da3229152cab9f14496b2d5a8b45268e1
Khoi Van <tien-khoi.van@mohawkcollege.ca>
2016-07-15 16:26:29 +0000

    finish

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
-app.get("/",function(req,res){
-    res.send("Hello World");
+app.get("/:userInput", function(req, res) {
+
+    var userInput = req.params.userInput;
+
+    if (Date.parse(userInput)) {
+        var date = moment(userInput)
+        var result = {
+            "unix": date.unix(),
+            "natural": date.format("MMMM D, YYYY")
+        };
+
+        res.json(result);
+    }
+    else if (userInput == parseInt(userInput, 10)) {
+        var date = moment.unix(userInput)
+
+
+        var result = {
+            "unix": date.unix(),
+            "natural": date.format("MMMM D, YYYY")
+        };
+        res.json(result);
+
+    }
+    else {
+        var result = {
+            "unix": null,
+            "natural": null
+        };
+        res.json(result);
+
+    }
+
 })
 
-app.listen(8080,function(){
-    console.log("Example on port 8080")
+app.listen(80, function() {
+   
 })
